eddeaaf4e85502a4eebe23c1558c7c7855426222
virgile <virgile.petermann@gmail.com>
2020-05-13 09:08:44 +0200

    bot en place

---
+++ chatbot.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const ReadLine = require('readline');
+const RiveScript = require('rivescript');
+const Chalk = require('chalk');
+
+const bot = new RiveScript();
+const readLine = ReadLine.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+bot.loadFile('./training_data.rive', function() {
+    console.log(Chalk.green("Succès de la lecture"));
+    bot.sortReplies();
+    ask();
+}, function(error) {
+    console.log("Erreur lors de la lecture du fichier : "+error);
+})
+
+function ask() {
+    readLine.question('Votre message : ' , (message) => {
+        reply = bot.reply('local-user', message).then(function(reply) {
+            console.log(Chalk.red("Bot : " + reply));
+        });
+    })
+}
