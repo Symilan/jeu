c56a94d3adc7ddfb46dff66cc914a83e77b2d164
shasolver <pleroy@enssat.fr>
2020-05-27 16:05:13 +0200

    début de code du bot discord

---
+++ botdiscord.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const PREFIX = "+";
+const Bot = require('./bot')
+
+module.exports = class BotDiscord{
+    constructor(){
+        client.login(NzE1MTgwNzIxNzk0MTg3Mjg2.Xs5o9A.s-TZUKTeoNfmMUXBgbaJxdJ9QzI);
+        this.bots = []; // Je met ici en place la liste des bots de discussions qui sont présents sur le serveur.
+    }
+    
+}
--- gestionDiscord.js
+++ gestionDiscord.js
@@ -1,21 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const PREFIX = "+";
-const Bot = require("bot.js")
+const BotDiscord = require("./botdiscord")
 const invitetoserv = "https://discord.com/api/oauth2/authorize?client_id=715180721794187286&permissions=1812462657&scope=bot"
 
 
 module.exports = class gestionnaireDiscord{
-    static addBotToServ(){
+    static inviteBotToServ(){
         /*Cette procédure permet de donner le lien d'invitation du bot au serveur. La personne voulant ajouter
         le bot se retrouve alors redirigé vers une page web qui lui donne la possibilité de le faire, ce qui permettra
         par la suite au bot d'intéragir avec la communauté comme il se doit. */
-        console.log("vous devez dans un premier temps inviter le bot sur votre serveur" + invitetoserv);
+        console.log("vous devez dans un premier temps inviter le bot sur votre serveur si ce n'est pas déjà fait en suivant le lien suivant : " + invitetoserv);
     }
 
-    static connect(name, brain, channel){
-        /*Cette fonction connecte le bot au serveur avec un nom et une personnalité / un cerveau donnés en argument
-        et lui indique sur quel channel sur serveur parler.*/
+    static connect(){
+        /*Cette procédure connecte le bot. Elle le "met en marche" pour qu'il puisse commencer à intéragir avec les utilisateurs.*/
+        this.inviteBotToServ();
+        this.bot = new BotDiscord();
     }
 
+    static addBot(name, brain, channel){
+        /*Cette fonction ajoute un bot au serveur avec un nom et une personnalité / un cerveau donnés en argument
+        et lui indique sur quel channel du serveur parler.*/
+        
+    }
 }
