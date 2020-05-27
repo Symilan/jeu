1a6d84b9076faba2f127e11db2c4435c5e4e2292
shasolver <pleroy@enssat.fr>
2020-05-27 15:01:57 +0200

    Ajout discord

---
+++ gestionDiscord.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const PREFIX = "+";
+const Bot = require("bot.js")
+
+
+module.exports = class gestionnaireDiscord{
+    constructor(){
+
+    }
+}
