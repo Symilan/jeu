45afa93922c7216c18786f9000bc793073284ebd
virgile <virgile.petermann@gmail.com>
2020-06-07 23:46:35 +0200

    merging

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ FileReader = require('./fileReader.js')
 module.exports = class Bot{
     constructor(id, brain, channel){
         this.id = id;
-        this.brain = FileReader.textFromPath(brain);
+        this.brain = brain;
         this.channel = channel;
     }
 
